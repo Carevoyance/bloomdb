6bddba9b8475e0745f60d81bfdba2309a29e510e
Daniel Imfeld <daniel@danielimfeld.com>
2014-12-15 00:04:42 -0600

    Add logging and revision tracking support

--- upsert.go
+++ upsert.go
@@ -5,8 +5,10 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/lib/pq"
+	"log"
 	"strings"
 	"text/template"
+	"time"
 )
 
 var fns = template.FuncMap{
@@ -19,42 +21,59 @@ var fns = template.FuncMap{
 }
 
 type upsertInfo struct {
-	Table     string
-	TempTable string
-	IdColumn  string
-	Columns   []string
+	Table        string
+	TempTable    string
+	IdColumn     string
+	HasRevisions bool
+	Columns      []string
 }
 
-func buildQuery(table string, tempTable string, idColumn string, columns []string) (string, error) {
-	buf := new(bytes.Buffer)
+func buildQuery(table string, tempTable string, idColumn string, columns []string, hasRevisions bool) (string, string, error) {
+	upsertBuf := &bytes.Buffer{}
 	t, err := template.New("upsert.sql.template").Funcs(fns).ParseFiles("sql/upsert.sql.template")
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
-	info := upsertInfo{table, tempTable, idColumn, columns}
-	err = t.Execute(buf, info)
+	info := upsertInfo{table, tempTable, idColumn, hasRevisions, columns}
+	err = t.Execute(upsertBuf, info)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
-	return buf.String(), nil
+
+	revisionBuf := &bytes.Buffer{}
+	if hasRevisions {
+		t, err = template.New("updaterevisions.sql.template").Funcs(fns).ParseFiles("sql/updaterevisions.sql.template")
+		if err != nil {
+			return "", "", err
+		}
+		err = t.Execute(revisionBuf, info)
+		if err != nil {
+			return "", "", err
+		}
+	}
+
+	return upsertBuf.String(), revisionBuf.String(), nil
 }
 
-func Upsert(db *sql.DB, table string, idColumn string, columns []string, rows chan []string) error {
+func Upsert(db *sql.DB, table string, idColumn string, columns []string, rows chan []string, hasRevisions bool) error {
 	// Can't create a temporary table inside a non-temporary schema, so just
 	// replace the periods, if present, with semicolons to avoid errors.
 	tempTable := strings.Replace(table, ".", "_", -1) + "_temp"
 
-	query, err := buildQuery(table, tempTable, idColumn, columns)
+	query, revisionQuery, err := buildQuery(table, tempTable, idColumn, columns, hasRevisions)
 	if err != nil {
 		return err
 	}
+
+	startTime := time.Now()
+	log.Println("Starting database write...")
 
 	txn, err := db.Begin()
 	if err != nil {
 		return err
 	}
 
-	_, err = txn.Exec("CREATE TEMP TABLE " + tempTable + "(LIKE " + table + ") ON COMMIT DROP;")
+	_, err = txn.Exec("CREATE TEMP TABLE " + tempTable + "(LIKE " + table + ")")
 	if err != nil {
 		return err
 	}
@@ -64,6 +83,7 @@ func Upsert(db *sql.DB, table string, idColumn string, columns []string, rows ch
 		return err
 	}
 
+	rowsProcessed := 0
 	for rawRow := range rows {
 		row := make([]interface{}, len(rawRow))
 		for i, column := range rawRow {
@@ -79,6 +99,12 @@ func Upsert(db *sql.DB, table string, idColumn string, columns []string, rows ch
 			fmt.Println("table", table, "row", row)
 			return err
 		}
+
+		rowsProcessed++
+
+		if rowsProcessed%100000 == 0 {
+			log.Printf("Processed %d rows...", rowsProcessed)
+		}
 	}
 
 	_, err = stmt.Exec()
@@ -86,15 +112,59 @@ func Upsert(db *sql.DB, table string, idColumn string, columns []string, rows ch
 		return err
 	}
 
-	_, err = txn.Exec(query)
+	err = stmt.Close()
 	if err != nil {
 		return err
 	}
 
+	// Use this transaction just for the copy, and start another one afterward
+	// for better performance.
 	err = txn.Commit()
 	if err != nil {
 		return err
 	}
 
+	endTime := time.Now()
+	duration := endTime.Sub(startTime)
+	duration = duration / time.Second
+	log.Printf("Processed %d rows total, took %d:%02d\n", rowsProcessed,
+		duration/60, duration%60)
+
+	log.Println("Creating table index")
+	_, err = db.Exec("CREATE UNIQUE INDEX ON " + tempTable + "(" + idColumn + ")")
+	if err != nil {
+		return err
+	}
+
+	log.Println("Analyzing table")
+	_, err = db.Exec("ANALYZE " + tempTable)
+
+	txn, err = db.Begin()
+	if err != nil {
+		return err
+	}
+
+	if revisionQuery != "" {
+		log.Println("Calculating revisions...")
+		res, err := txn.Exec(revisionQuery)
+		if err != nil {
+			return err
+		}
+		affected, _ := res.RowsAffected()
+		log.Printf("Updated revision on %d rows", affected)
+	}
+
+	log.Println("Performing upsert...")
+	_, err = txn.Exec(query)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Committing transaction...")
+	err = txn.Commit()
+	if err != nil {
+		return err
+	}
+	log.Println("Done")
 	return nil
 }
